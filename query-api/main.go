3731f29a9f16ee02261a0e4dcde02006fe671d8e
Alex Lewis <alexlewisnc@gmail.com>
2021-10-06 16:51:27 -0400

    Have queries return a list of collection names

--- query-api/main.go
+++ query-api/main.go
@@ -6,27 +6,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	db_connect()
+type QueryHandler struct {
+	db *mongo.Database
+}
 
-	wd, _ := os.Getwd()
-	log.Printf("working directory: %s\n", wd)
+func main() {
+	client, db := db_connect()
+	defer client.Disconnect(context.TODO())
+	http.Handle("/", &QueryHandler{db})
 
 	PORT := ":8080"
 	log.Printf("Running server on %s\n", PORT)
-	// http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Sprintf("%s %s", r.Method, r.URL.Path)))
-	})
 	log.Fatal(http.ListenAndServe(PORT, nil))
 }
 
-func db_connect() {
+func db_connect() (*mongo.Client, *mongo.Database) {
 	DB_HOST := os.Getenv("MONGODB_SERVICE_HOST")
 	DB_PORT := os.Getenv("MONGODB_SERVICE_PORT")
 	DB_USER := os.Getenv("MONGODB_USER")
@@ -35,10 +36,32 @@ func db_connect() {
 	DB_URI := fmt.Sprintf("mongodb://%s:%s@%s:%s/", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(DB_URI))
 	if err != nil {
-		// TODO
-		log.Fatal("@@@ failed to connect to MongoDB\n")
+		log.Fatal("@@@ failed to connect to MongoDB\n") // TODO
 	}
 	db := client.Database(DB_NAME)
-	log.Printf("db: %v\n", db)
-	client.Disconnect(context.TODO())
+	return client, db
+}
+
+func (handler *QueryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Path[1:]
+	collections, err := handler.db.ListCollectionNames(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err) // TODO
+	}
+	found := false
+	for _, name := range collections {
+		if query == name {
+			found = true
+			break
+		}
+	}
+
+	var message string
+	if found {
+		message = fmt.Sprintf("`%s` found in collections", query)
+	} else {
+		message = fmt.Sprintf("`%s` not found in collections", query)
+	}
+	message = fmt.Sprintf("%s %s\n\n%s: {%s}", r.Method, r.URL.Path, message, strings.Join(collections, ","))
+	w.Write([]byte(message))
 }
