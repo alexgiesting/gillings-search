44eb96250a209ce1884e55f1d95eb0408688a11b
Alex Lewis <alexlewisnc@gmail.com>
2021-10-31 17:26:53 -0400

    Rewrite index script to use some React components

--- static/script.js
+++ static/script.js
@@ -1,65 +1,97 @@
 ReactDOM.render(
-  <App message="Gillings Search Tool" />,
+  <App header="Gillings Search Tool" />,
   document.getElementById("root")
 );
 
-function App(props) {
+function App({ header }) {
   return (
     <div>
-      <h1>{props.message}</h1>
+      <h1>{header}</h1>
 
-      <p>
-        <form action="/query">
-          <label for="faculty">surname</label>
-          <input name="faculty" type="text" />
-          <br />
-          <label for="keyword">keyword</label>
-          <input name="keyword" type="text" />
-          <br />
-          <input type="submit" value="Search" />
-        </form>
-        <br />
-      </p>
+      <QueryForm />
+      <br />
 
-      <p>
-        <form
-          method="post"
-          action="/update/load/faculty"
-          enctype="multipart/form-data"
-        >
-          <label for="file">upload faculty.csv: </label>
-          <input name="file" type="file" />
-          <br />
-          <label for="key">(password)</label>
-          <input name="key" type="text" />
-          <input type="submit" value="Upload" />
-        </form>
-        <form
-          method="post"
-          action="/update/load/themes"
-          enctype="multipart/form-data"
-        >
-          <label for="file">upload themes.xml: </label>
-          <input name="file" type="file" />
-          <br />
-          <label for="key">(password)</label>
-          <input name="key" type="text" />
-          <input type="submit" value="Upload" />
-        </form>
-      </p>
+      <LoadForm to="faculty" from="faculty.csv" />
+      <LoadForm to="themes" from="themes.xml" />
+      <br />
 
-      <p>
-        <form action="/update/drop/citations">
-          <label for="key">(password)</label>
-          <input name="key" type="text" />
-          <input type="submit" value="Drop citations" />
-        </form>
-        <form action="/update/pull">
-          <label for="key">(password)</label>
-          <input name="key" type="text" />
-          <input type="submit" value="Pull citations" />
-        </form>
-      </p>
+      <Update label="Drop citations" endpoint="drop/citations" />
+      <Update label="Pull citations" endpoint="pull" />
     </div>
+  );
+}
+
+function QueryForm({}) {
+  // const [v, setV] = React.useState(3);
+  return (
+    <form
+      onSubmit={async function (event) {
+        event.preventDefault();
+        const request = {};
+        [...event.target]
+          .filter((input) => input.value != "" && input.name != "")
+          .forEach((input) => {
+            request[input.name] = input.value.split(",").map((w) => w.trim());
+          });
+        const result = await fetch(
+          `/query?q=${encodeURIComponent(JSON.stringify(request))}`,
+          {
+            method: "GET",
+          }
+        ).then((response) => response.json());
+        console.log(result);
+      }}
+    >
+      <QueryText name="faculty" label="surnames" />
+      <br />
+      <QueryText name="keyword" label="keywords" />
+      <br />
+
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
+
+function QueryText({ name, label }) {
+  return (
+    <label>
+      {label}: <input name={name} type="text" />
+    </label>
+  );
+}
+
+function LoadForm({ to, from }) {
+  return (
+    <form
+      method="post"
+      action={`/update/load/${to}`}
+      encType="multipart/form-data"
+    >
+      <label>
+        upload {from}: <input name="file" type="file" />
+      </label>
+      <br />
+      <label>
+        (password) <input name="key" type="text" />
+      </label>
+      <input type="submit" value="Upload" />
+    </form>
+  );
+}
+
+function Update({ endpoint, label }) {
+  return (
+    <form action={`/update/${endpoint}`}>
+      <Password />
+      <input type="submit" value={label} />
+    </form>
+  );
+}
+
+function Password({}) {
+  return (
+    <label>
+      (password) <input name="key" type="text" />
+    </label>
   );
 }
