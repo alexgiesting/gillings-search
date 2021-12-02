bd15adc4cd84a86bb8a0a58cdf3124765a10dcd5
chun-andrew <49577025+chun-andrew@users.noreply.github.com>
2021-12-01 21:48:45 -0500

    Frontend stuff

--- react-app/src/components/ArticleCard.js
+++ react-app/src/components/ArticleCard.js
@@ -1,13 +1,69 @@
 import React from "react";
+import ArticleCardActions from "./ArticleCardActions";
 
-function ArticleCard({ document }) {
-  return (
-    <div className="article-card">
-      <p>{document.Title}</p>{" "}
-      <p>({document.Authors.map((author) => author.Name).join(", ")})</p>
-      {/* TODO: Other information displayed on the cards */}
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+    editButton: {
+        marginRight: 8,
+        display: 'inline',
+    },
+    cancelButton: {
+        float: 'right',
+        color: '#34495E',
+    },
+    gridContainer: {
+        paddingTop: theme.spacing(4),
+    },
+    importantCell: {
+        fontWeight: 600,
+        fontSize: 18,
+    },
+    infoIcon: {
+        verticalAlign: 'middle',
+    }
+}));
+
+export default function ArticleCard({ document, removeCitation, addCitation }) {
+    const theme = useTheme();
+    const classes = useStyles(theme);
+
+    const citationRow = (document) => {
+        return (
+            <TableRow className="article-card" key={document.id}>
+                {/* Title */}
+                <TableCell className={classes.importantCell}>{document.Title}</TableCell>
+
+                {/* Author(s) */}
+                <TableCell>
+                    {document.Authors.map((author) => author.Name).join(", ")}
+                </TableCell>
+  
+                {/* Tooltip Blurb? */}
+                {/* <TableCell>{row.WrapRateMultiplier ? row.WrapRateMultiplier : defaultWrapRateMultiplier}</TableCell> */}
+
+                <TableCell align="right">
+                    <ArticleCardActions
+                        document={document} 
+                        addCitation={addCitation} 
+                        removeCitation={removeCitation} />
+                </TableCell>
+            </TableRow>
+        );
+    }
+
+    return (
+        <React.Fragment>
+            {
+                // citationTable.length > 0 ?
+                (<Table size="medium">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell width="16.25%">Title</TableCell>
+                            <TableCell width="12%">Author(s)</TableCell>
+                            <TableCell width="7.5%">Blurb</TableCell>
+                        </TableRow>
+                    </TableHead>
+                </Table>) 
+            }
+        </React.Fragment>
+    );
 }
-
-export default ArticleCard;
