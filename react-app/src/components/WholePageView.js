90715613bf9608d33dd65644e60bc31e8f81dc91
chun-andrew <49577025+chun-andrew@users.noreply.github.com>
2021-12-28 17:43:48 -0500

    Formatting
    Quick formatting push

--- react-app/src/components/WholePageView.js
+++ react-app/src/components/WholePageView.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 
-import { QueryForm, LoadForm, Update, ArticleOfTheDay } from "./helpers/helper_functions";
+import {
+  QueryForm,
+  LoadForm,
+  Update,
+  ArticleOfTheDay,
+} from "./helpers/helper_functions";
 
 import "../App.css";
 import ArticleCard from "./ArticleCard";
 
 function WholePageView({ _ }) {
   const [results, setResults] = React.useState([]);
-  
+
   return (
     <div className="base">
       <div className="navi">
@@ -73,17 +78,24 @@ function WholePageView({ _ }) {
               ))}
           </div>
 
-          <div className = "articleOfTheDay" >
-              {results.length == 0 ? <>
-              <h1>Article of the Day</h1>
-              <p><a className = "ArticleLink" href="https://projecteuclid.org/journals/statistical-science/volume-13/issue-2/Statistical-advances-in-environmental-science/10.1214/ss/1028905935.full">Statistical advances in environmental science</a></p>
-              <p>(Piegorsch W.W., Smith E.P., Edwards D., Smith R.L.)</p>
+          <div className="articleOfTheDay">
+            {results.length == 0 ? (
+              <>
+                <h1>Article of the Day</h1>
+                <p>
+                  <a
+                    className="ArticleLink"
+                    href="https://projecteuclid.org/journals/statistical-science/volume-13/issue-2/Statistical-advances-in-environmental-science/10.1214/ss/1028905935.full"
+                  >
+                    Statistical advances in environmental science
+                  </a>
+                </p>
+                <p>(Piegorsch W.W., Smith E.P., Edwards D., Smith R.L.)</p>
               </>
-              : 
-              
-              <></>}
+            ) : (
+              <></>
+            )}
           </div>
-
         </div>
       </div>
     </div>
